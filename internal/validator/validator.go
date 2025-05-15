d204ce6f6303470953a795254a1389f441aa1dd1
HamzaAkb <hamzawakbar@gmail.com>
2025-05-15 15:23:05 +0500

    refactor: rename PermittedInt to PermittedValue for improved type flexibility

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -41,14 +41,14 @@ func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
 }
 
-func PermittedInt(value int, permittedValues ...int) bool {
-	for i := range permittedValues {
-		if value == permittedValues[i] {
-			return true
-		}
-	}
-	return false
-}
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+    for i := range permittedValues {
+        if value == permittedValues[i] {
+            return true
+        }
+    }
+    return false
+ }
 
 func MinChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) >= n
